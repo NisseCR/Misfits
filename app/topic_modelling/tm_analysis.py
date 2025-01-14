0ea316c5ae46ce7191c231b76614dc1a372ea15f
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2025-01-14 20:20:26 +0100

    Added code for preprocessing, running the LDA topic modelling, and running k-means clustering

--- app/topic_modelling/tm_analysis.py
+++ app/topic_modelling/tm_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 from gensim.corpora import Dictionary
 from gensim.models import CoherenceModel, LdaModel
 from gensim import corpora
@@ -9,27 +10,33 @@ def _read_data() -> pd.DataFrame:
     return pd.read_csv('./data/tm_preprocess.csv')
 
 
-def _create_lda_model(id2word, corpus, num_topics: int) -> LdaModel:
-    return LdaModel(
-        corpus=corpus,
-        id2word=id2word,
-        num_topics=num_topics,
-        alpha='symmetric',
-        eta='symmetric',
-        random_state=123
-    )
+def _compute_tfidf(df: pd.DataFrame) -> tuple[pd.DataFrame, list[str]]:
+    """
+    Compute the TF-IDF matrix to identify the most relevant words for LDA,
+    and to apply the k-means clustering.
+    """
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(df['text_preprocess'])
+    feature_names = vectorizer.get_feature_names_out()
+    tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=feature_names)
+    return tfidf_df, feature_names
 
 
-def _setup_lda_data(df: pd.DataFrame) -> tuple[Dictionary, list[list[tuple[int, int]]]]:
-    # Create word2id dictionary.
-    comments = df['text_preprocess'].apply(lambda x: x.split(' ')).copy()
+# Based on the tfidf matrix, filter out words for LDA based on thresholds.
+def _filter_by_tfidf(tfidf_df: pd.DataFrame, feature_names: list,
+                     low_threshold: float, high_threshold: float) -> list[str]:
+    feature_scores = tfidf_df.mean(axis=0)
+    keep_features = [feature_names[i] for i, score in enumerate(feature_scores)
+                     if low_threshold <= score <= high_threshold]
+    return keep_features
+
+
+# Create an id2word representation and corpus for LDA model
+def _setup_lda_data(df: pd.DataFrame, relvant_features: list) -> (
+        tuple)[Dictionary, list[list[tuple[int, int]]]]:
+    comments = df['text_preprocess'].apply(
+            lambda x: [word for word in x.split() if word in relvant_features])
     id2word = corpora.Dictionary(comments.values)
-
-    # Filter out extremes to limit the number of features.
-    id2word.filter_extremes(
-        no_below=2,
-        no_above=0.95
-    )
 
     # Create BoW collection.
     corpus = [id2word.doc2bow(doc) for doc in comments.values]
@@ -37,20 +44,53 @@ def _setup_lda_data(df: pd.DataFrame) -> tuple[Dictionary, list[list[tuple[int, 
     return id2word, corpus
 
 
+def _create_lda_model(id2word, corpus, num_topics: int) -> LdaModel:
+    return LdaModel(
+            corpus=corpus,
+            id2word=id2word,
+            num_topics=num_topics,
+            alpha='auto',
+            random_state=123,
+            per_word_topics=True
+    )
+
+
+def _get_dominant_topic(lda_model, corpus) -> list[int]:
+    dominant_topics = []
+    for doc in corpus:
+        topics = lda_model.get_document_topics(doc)
+        dominant_topic = max(topics, key=lambda x: x[1])[0]
+        dominant_topics.append(dominant_topic)
+    return dominant_topics
+
+
+def _kmeans_clustering(tfidf_df: pd.DataFrame,
+                       relevant_features: list,
+                       num_clusters: int) -> list[int]:
+    # Use the same features as for LDA.
+    relevant_tfidf = tfidf_df[relevant_features]
+    kmeans = KMeans(n_clusters=num_clusters, random_state=123)
+    kmeans.fit(tfidf_df)
+    return kmeans.labels_
+
+
 def _write_data(df: pd.DataFrame) -> None:
     df.to_csv('./data/tm_topics.csv', index=False)
 
 
-def analyse() -> None:
+def analyse():
     df = _read_data()
-    id2word, corpus = _setup_lda_data(df)
+
+    tfidf_df, feature_names = _compute_tfidf(df)
+    relevant_features = _filter_by_tfidf(tfidf_df, feature_names, 0.002,
+                                         0.007)
+
+    id2word, corpus = _setup_lda_data(df, relevant_features)
     lda_model = _create_lda_model(id2word, corpus, 7)
+    df['dominant_topic'] = _get_dominant_topic(lda_model, corpus)
 
-    # TODO doesn't full work yet i think, but brain is now deadge
-    # TODO apart from getting global topics, we should also assign topics to each entry (or perhaps only the dominant topic, as Dennis mentioned).
-    # TODO choose word representation and model for topic modelling.
+    kmeans_labels = _kmeans_clustering(tfidf_df, relevant_features, 7)
+    df['kmeans_cluster'] = kmeans_labels
 
-    # TODO remove this dummy result with actual model results.
-    df['dominant_topic'] = 'd&d'
-
+    print(df)
     _write_data(df)
--- app/topic_modelling/tm_preprocessing.py
+++ app/topic_modelling/tm_preprocessing.py
@@ -2,43 +2,58 @@ import pandas as pd
 import spacy
 import nltk
 import string
+import re
 from nltk.corpus import stopwords
-
+from nltk.stem import PorterStemmer
 
 # Retrieve pre-defined stop words.
 nltk.download('stopwords')
-stop_words = stopwords.words('english')
+default_stop_words = set(stopwords.words('english'))
 
-# Load spacy corpus.
+# Load spacy corpus and define stemmer
 nlp = spacy.load('en_core_web_sm')
+stemmer = PorterStemmer()
+
+# Define custom stopwords
+custom_stopwords = {'ghana', 'nigeria', 'country', 'countries', 'city'}
 
 
 def _read_data() -> pd.DataFrame:
     return pd.read_csv('./data/filtered.csv')
 
 
-def _preprocess_text(text: list):
-    # TODO do proper preprocessing steps for topic modelling.
-    include_features = ['ADJ', 'INTJ', 'NOUN', 'PROPN', 'VERB']
+def _preprocess_text(text: list) -> str:
+    """
+    Methond to preprocess text data. The method performs the following steps:
+    1. Lemmatize text and isolate nouns.
+    2. Remove stop words.
+    3. Remove punctuation, numbers, special characters, and extra whitespaces.
+    4. Stemming.
+    """
+    include_features = ['NOUN']
+    all_stop_words = default_stop_words.union(custom_stopwords)
 
-    # Lemmatization, stop-word removal and POS feature selection.
-    text = ' '.join([ent.lemma_.strip() for ent in text if ent.pos_ in include_features and ent.text not in stop_words])
+    # Lemmatize text and remove stop words.
+    text = ' '.join([ent.lemma_.strip() for ent in text
+                     if ent.pos_ in include_features and ent.text
+                     not in all_stop_words])
 
-    # Remove punctuation.
-    text = text.translate(str.maketrans(' ', ' ', string.punctuation))
+    # Remove punctuation, numbers, special characters, and extra whitespaces.
+    text = text.translate(str.maketrans('', '',
+                                        string.punctuation + string.digits))
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = text.strip().lower()
 
-    # Remove numbers.
-    text = text.translate(str.maketrans(' ', ' ', string.digits))
-
-    # Enforce lower-casing.
-    text = text.lower()
+    # Stemming
+    text = ' '.join([stemmer.stem(word) for word in text.split()])
 
     return text
 
 
 def _preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     df['text_preprocess'] = df['text'].apply(nlp)
-    df['text_preprocess'] = df['text_preprocess'].apply(_preprocess_text)
+    df['text_preprocess'] = df['text_preprocess'].apply(
+            lambda text: _preprocess_text(text))
     return df
 
 
